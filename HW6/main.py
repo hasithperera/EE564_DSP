f081af2f2364bf57e11bf692e09140d9ab39248d
Hasith Perera <12579141+hasithperera@users.noreply.github.com>
2023-05-02 08:47:20 -0400

    modified by adding q3

--- HW6/main.py
+++ HW6/main.py
@@ -17,24 +17,51 @@ def DFT(x,k):
     
 if __name__=='__main__':
     
-   
-    t = np.linspace(0,5,2**4)
+    N = 2**6
+    t = np.linspace(0,5,N)
     dt = t[1]-t[0]
-    df = 1 / dt
-    f = 2
-    y = 255*(.5*np.sin(2*np.pi*t*f)+.5)
+    f = 1 / dt
+
+    
+    # range +/- 128 signed 8 bits
+    # exact 3rd bin
+    f0 = 3*f/N
+    y = 128*(np.sin(2*np.pi*t*f0))
+    
+    # range +/- 64
+    # close to 7
+    
+    f1 = 7*f/N + .1
+    y1 = 64*(np.sin(2*np.pi*t*f1))
+    
+    # noise
+    snr= .2
+    ns = 128*(2*(np.random.rand(N)-.5))*snr
+    
+    y = y + y1
+    y_ns = y + ns
+    
     
     fft = []
+    fft2 = []
     
     for i in range(0,len(t)):
         fft.append(DFT(y,i))
+        fft2.append(DFT(y_ns,i))
 #    print(a)
-    
+    plt.figure(1)
+    plt.subplot(211)
     plt.plot(t,y)
-    plt.figure()
-    plt.plot(np.abs(fft))
-    plt.plot(np.abs(np.fft.fft(y)),'o')
+    plt.plot(t,y_ns)
+    plt.legend(['signal','signal with noise'])
     
-    plt.legend(['my DFT','np.fft.fft'])
+    
+#    plt.figure(2)
+    plt.subplot(212)
+    plt.plot(np.abs(fft))
+    plt.plot(np.abs(fft2))
+#    plt.plot(np.abs(np.fft.fft(y)),'o')
+    
+    plt.legend(['signal','signal with noise'])
     
 #    print(fft)
